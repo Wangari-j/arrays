24789eb936997ae3e95b1013195264242b10c850
Wangari-j <wangarijudy@gmail.com>
2024-02-11 23:40:39 +0300

    arrays

---
+++ arrays.py
@@ -0,0 +1,12 @@
+names = ["Angie",  "Bien", "Lulu","Cate", "Dean", "Eve", "Fay"]
+
+age = [22, 28, 30, 26, 23, 20]
+
+for c in names:
+    print(c)
+
+for y in names:
+    print("My name is " + y)
+
+for x in names:
+    print("Hello " + x)
